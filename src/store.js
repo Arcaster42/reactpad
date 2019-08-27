5f00a157a5b94f7d242ff0c9b04310a07a3520fa
TJ Epperson <tj.epperson070@gmail.com>
2019-08-27 19:51:55 +0900

    implemented store

---
+++ src/store.js
@@ -0,0 +1,24 @@
+import { createStore } from 'redux'
+
+//initial state
+const initialState = {
+    notes: [{ id: 0, title: 'Test Note', text: 'This is a test note in the redux store.' }]
+}
+
+//reducer
+function reducer(state = initialState, action) {
+    if (action.type === 'ADD_NOTE') {
+        return { ...state, notes: [...action.payload] }
+    }
+    return state
+}
+
+//actions
+function addNote(payload) {
+    return { type: 'ADD_NOTE', payload }
+}
+
+//store
+const store = createStore(reducer)
+
+export default store
