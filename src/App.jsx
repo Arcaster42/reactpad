31ecef50bbf5ead65f9c61947e388e1375e42d42
TJ Epperson <tj.epperson070@gmail.com>
2019-08-27 19:51:37 +0900

    implemented List component

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import List from './components/List'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Notes</h2>
+        <List />
+      </header>
+    </div>
+  );
+}
+
+export default App;
