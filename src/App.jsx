d6b5eeaf340987eb28fe67b6492a305fb327643a
TJ Epperson <tj.epperson070@gmail.com>
2019-08-28 10:12:42 +0900

    added pad form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import List from './components/List'
+import Pad from './components/Pad'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <header className="App-header">
         <h2>Notes</h2>
         <List />
+        <Pad />
       </header>
     </div>
   );
